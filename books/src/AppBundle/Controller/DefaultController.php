dd384ff3ef36f04f232171458d16c70d38f45bb2
Vladislav Ivchencov <ivchenov@gmail.com>
2017-11-09 21:33:39 +0300

    Контроллер главной страницы

--- books/src/AppBundle/Controller/DefaultController.php
+++ books/src/AppBundle/Controller/DefaultController.php
@@ -5,17 +5,48 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\Book;
+use AppBundle\Entity\Author;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Common\Persistence\ManagerRegistry;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/", name="main")
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
+    	$em = $this->get('doctrine')->getManager();
+		$repository = $em->getRepository('AppBundle:Book');
+		$book = $repository->findAll();
+		$arr=array();
+		// Собироем данные для вывода ккниг
+		foreach ($book as $value) {
+ 		
+ 			$auth=array();
+			$arr[$value->getId()]['id']=$value->getId();
+			$arr[$value->getId()]['name']=$value->getName();
+			 foreach ($value->getAuthor() as $val1) {
+			$arr[$value->getId()]['auth'][]=$val1->getName();
+			}
+			$arr[$value->getId()]['isbn']=$value->getIsbn();
+			$arr[$value->getId()]['pages']=$value->getPages();
+			$arr[$value->getId()]['year']=$value->getYear();
+			$arr[$value->getId()]['pic']=$value->getPic();
+		}
+		$repository = $em->getRepository('AppBundle:Author');
+		$auth = $repository->findAll();
+		
+		$arr2=array();
+		// Собироем данные для вывода авторов
+		foreach ($auth as $value) {
+			$arr2[$value->getId()]['id']=$value->getId();
+			$arr2[$value->getId()]['name']=$value->getName();
+			}		
+
+
+        //
+        return $this->render('default/index.html.twig', array('book' => $arr, 'auth' => $arr2));
     }
 }
